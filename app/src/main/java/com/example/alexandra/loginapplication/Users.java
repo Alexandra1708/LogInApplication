e4d5d34c7653491a859c9c8d003806aa752f96f0
Stefan Buzoianu <stefan.buzoianu@synergyse.eu>
2018-01-05 11:13:56 +0200

    activity_main updated

--- app/src/main/java/com/example/alexandra/loginapplication/Users.java
+++ app/src/main/java/com/example/alexandra/loginapplication/Users.java
@@ -2,31 +2,45 @@ package com.example.alexandra.loginapplication;
 
 
 public class Users {
-    int id;
-    String username, password;
+    private int id;
+    private String username;
+    private String email;
+    private String password;
 
     public void setId(int id)
     {
+
         this.id=id;
     }
     public int getId()
     {
-        return this.id;
+
+        return id;
     }
     public void setUsername(String username)
     {
+
         this.username=username;
     }
     public String getUsername()
     {
-        return this.username;
+
+        return username;
     }
     public void setPassword(String password)
     {
+
         this.password=password;
     }
     public String getPassword()
     {
-        return this.password;
+
+        return password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return  email;
     }
 }
